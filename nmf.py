15e843fc60bb4d83d1d2f1795e8a681fae6bd3fc
Florian Le Bronnec <flb@pop-os.localdomain>
2022-04-13 16:31:14 +0200

    Commented nmf.py.

--- nmf.py
+++ nmf.py
@@ -1,42 +1,49 @@
+from typing import Tuple
+
 import numpy as np
 
 from kmeans import kmeans_pp_init
 
 
-def logdot(a, b):
-    max_a, max_b = np.max(a), np.max(b)
-    exp_a, exp_b = a - max_a, b - max_b
-    np.exp(exp_a, out=exp_a)
-    np.exp(exp_b, out=exp_b)
-    c = np.dot(exp_a, exp_b)
-    np.log(c, out=c)
-    c += max_a + max_b
-    return c
-
-
 class NMF:
     def __init__(
-        self, n_clusters, max_iter=5000, tolerance=1e-7, seed=0, init="random"
+        self,
+        n_clusters: int,
+        max_iter: int = 5000,
+        tolerance: float = 1e-7,
+        seed: int = 0,
+        init: str = "random",
     ):
+        """2-NMF $X = FG^T$ with orthogonality constraints on G.
+
+        Args:
+            n_clusters (int): The number of dictionary atoms.
+            max_iter (int, optional): The maximum number of iterations of the algorithm. Defaults to 5000.
+            tolerance (float, optional): The minimum relative error decrease to reach before stopping the iterations. Defaults to 1e-7.
+            seed (int, optional): Random seed. Defaults to 0.
+            init (str, optional): Which initialization to use. One of "random", "kmeans++". Defaults to "random".
+        """
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.tolerance = tolerance
         self.seed = seed
         self.init = init
 
-    def fit(self, X):
+    def fit(self, X: np.ndarray):
+        """Fit the NMF with orthogonality constraints.
+
+        Args:
+            X (np.ndarray): The samples on which to fit the NMF.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray]: F and G factor matrices of X.
+        """
         # Transpose to get the classical NMF problem.
         X = X.T
         p, N = X.shape
         rng = np.random.default_rng(seed=self.seed)
         if self.init == "random":
             F = rng.uniform(size=(p, self.n_clusters))
-            G = rng.uniform(size=(N, self.n_clusters))
-        if self.init == "centered":
-            F = np.zeros((p, self.n_clusters))
-            for i in range(p):
-                min_val, max_val = X[i, :].min(), X[i, :].max()
-                F[i, :] = rng.uniform(min_val, max_val, size=(self.n_clusters))
             G = rng.uniform(size=(N, self.n_clusters))
 
         if self.init == "kmeanspp":
@@ -65,7 +72,15 @@ class NMF:
         self.list_errors = list_errors
         return self
 
-    def fit_predict(self, X):
+    def fit_predict(self, X: np.ndarray):
+        """A wrapper to fit and predict samples.
+
+        Args:
+            X (np.ndarray): The samples on which to fit the NMF.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray]: F and G factor matrices of X.
+        """
         self.fit(X)
         labels = self.G.argmax(axis=1)
         return self.F, self.G, labels
@@ -74,13 +89,23 @@ class NMF:
 class TriNMF:
     def __init__(
         self,
-        n_clusters,
-        max_iter=5000,
-        tolerance=1e-7,
-        diagonal=True,
-        seed=0,
-        init="random",
+        n_clusters: int,
+        max_iter: int = 5000,
+        tolerance: float = 1e-7,
+        diagonal: bool = True,
+        seed: int = 0,
+        init: str = "random",
     ):
+        """3-NMF $X = F S G^T$, with orthogonality constraints on F and G.
+
+        Args:
+            n_clusters (int): The number of dictionary atoms.
+            max_iter (int, optional): The maximum number of iterations of the algorithm. Defaults to 5000.
+            tolerance (float, optional): The minimum relative error decrease to reach before stopping the iterations. Defaults to 1e-7.
+            diagonal (bool, optional): _description_. Defaults to True.
+            seed (int, optional): Random seed. Defaults to 0.
+            init (str, optional): Which initialization to use. One of "random", "kmeans++". Defaults to "random".
+        """
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.tolerance = tolerance
@@ -88,7 +113,16 @@ class TriNMF:
         self.init = init
         self.diagonal = diagonal
 
-    def fit(self, X):
+    def fit(self, X: np.ndarray):
+
+        """Fit the 3-NMF with orthogonality constraints.
+
+        Args:
+            X (np.ndarray): The samples on which to fit the NMF.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray, np.ndarray]: F, S and G factor matrices of X.
+        """
         # Transpose to get the classical NMF problem.
         X = X.T
         p, N = X.shape
@@ -145,7 +179,15 @@ class TriNMF:
         self.S = S
         self.list_errors = list_errors
 
-    def fit_predict(self, X):
+    def fit_predict(self, X: np.ndarray):
+        """A wrapper to fit and predict samples.
+
+        Args:
+            X (np.ndarray): The samples on which to fit the NMF.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray, np.ndarray]: F, S and G factor matrices of X.
+        """
         self.fit(X)
         labels = self.G.argmax(axis=1)
         return self.F, self.S, self.G, labels
