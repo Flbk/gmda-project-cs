9af5486be19e5a7288f77a0b21563d03a29a6160
Florian Le Bronnec <flb@pop-os.localdomain>
2022-04-13 15:51:15 +0200

    Doc for visualization.py.

--- visualisation.py
+++ visualisation.py
@@ -1,17 +1,27 @@
 from pathlib import Path
+from typing import Iterable
 
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
+
 pio.kaleido.scope.mathjax = None
 cmap = px.colors.qualitative.Plotly
 
 
-def save_fig(fig, file_name, dir_path, show=False):
+def save_fig(fig: go.Figure, file_name: str, dir_path: str, show: bool = False):
+    """Save and print plotly figures.
+
+    Args:
+        fig (go.Figure): The plotly figure to save.
+        file_name (str): File name.
+        dir_path (str): Directory where to save the file.
+        show (bool, optional): Whether to show the figure. Defaults to False.
+    """
     if show:
         fig.show()
-    if file_name != "":
+    if dir_path_name != None:
         save_file = Path(dir_path) / file_name
         save_file.parent.mkdir(exist_ok=True, parents=True)
         pio.full_figure_for_development(fig, warn=False)
@@ -20,8 +30,29 @@ def save_fig(fig, file_name, dir_path, show=False):
 
 
 def plot_centers(
-    samples, centers, name="", color="red", title="", width=700, height=300
+    samples: np.ndarray,
+    centers: np.ndarray,
+    name: str = "",
+    color: str = "red",
+    title: str = "",
+    width: int = 700,
+    height: int = 300,
 ):
+    """Plot a dataset of 2D points and highlight some points.
+    In most cases these highlighted points will be interpreted as cluster's centers.
+
+    Args:
+        samples (np.ndarray, (N, 2)): The 2D dataset.
+        centers (np.ndarray, (M, 2)): M 2D cluster's centers.
+        name (str, optional): The name of the dataset for the legend. Defaults to "".
+        color (str, optional): The color for the centers. Defaults to "red".
+        title (str, optional): The title of the figure. Defaults to "".
+        width (int, optional): Width in px. Defaults to 700.
+        height (int, optional): Height in px. Defaults to 300.
+
+    Returns:
+        go.Figure: The plotly figure.
+    """
     fig = go.Figure()
     trace_X = go.Scatter(
         x=samples[:, 0],
@@ -55,8 +86,28 @@ def plot_centers(
 
 
 def plot_several_centers(
-    samples, list_centers, list_names, list_colors, width=400, height=300, title=None
+    samples: np.ndarray,
+    list_centers: Iterable[np.ndarray],
+    list_names: Iterable[str],
+    list_colors: Iterable[str],
+    width: int = 400,
+    height: int = 300,
+    title: str = None,
 ):
+    """Plot several highlighted batch of centers on top of a 2D dataset.
+
+    Args:
+        samples (np.ndarray): _description_
+        list_centers (Iterable[np.ndarray]): List of (M, 2) arrays containing centers from different ouputs.
+        list_names (Iterable[str]): List of legend names of each batch of centers.
+        list_colors (Iterable[str]): The colors for each batch of center.
+        width (int, optional): Figure's width. Defaults to 400.
+        height (int, optional): Figure's height. Defaults to 300.
+        title (str, optional): Figure's title. Defaults to None.
+
+    Returns:
+        go.Figure: The plotly figure.
+    """
     fig = plot_centers(
         samples,
         list_centers[0],
@@ -82,7 +133,20 @@ def plot_several_centers(
     return fig
 
 
-def plot_clustering(X, labels, width=400, height=300):
+def plot_clustering(
+    X: np.ndarray, labels: np.ndarray, width: int = 400, height: int = 300
+):
+    """Plot a clustering (i.e each point in the same cluster will have the same color).
+
+    Args:
+        X (np.ndarray, (N, 2)): The 2D dataset.
+        labels (np.ndarray, (N,)): The labels of each point.
+        width (int, optional): The width of the figure.. Defaults to 400.
+        height (int, optional): The height of the figure. Defaults to 300.
+
+    Returns:
+        go.Figure: The plotly figure.
+    """
     ind_sort = np.argsort(labels)
     X = X[ind_sort]
     labels = labels[ind_sort]
